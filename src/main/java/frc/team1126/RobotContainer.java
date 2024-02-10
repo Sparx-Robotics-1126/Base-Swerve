d0248591301c34f69549d70b413b39e7f484183c
flareware95 <flareware95@gmail.com>
2024-02-09 23:12:48 -0500

    Revert "updates for climber"
    This reverts commit 64000d280bec6c1c3bc293ebab8894393f98eb6b.

--- src/main/java/frc/team1126/RobotContainer.java
+++ src/main/java/frc/team1126/RobotContainer.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.util.HashMap;
 
 import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -19,11 +21,13 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.team1126.Constants.SwerveConstants;
 import frc.team1126.commands.Limelight.DriveToDistance;
-import frc.team1126.commands.Limelight.LLAlignCommand;
+// import frc.team1126.commands.Limelight.LLAlignCommand;
 import frc.team1126.commands.Limelight.VisionAlignment;
-import frc.team1126.commands.drive.DriveFieldRelative;
+// import frc.team1126.commands.drive.DriveFieldRelative;
+
 import frc.team1126.subsystems.CANdleSubsystem;
-import frc.team1126.subsystems.SwerveSubsystem;
+import frc.team1126.subsystems.Climber;
+//import frc.team1126.subsystems.SwerveSubsystem;
 import frc.team1126.subsystems.sensors.Limelight;
 public class RobotContainer 
 {
@@ -32,68 +36,66 @@ public class RobotContainer
   private final int strafeAxis = XboxController.Axis.kLeftX.value;
   private final int rotationAxis = XboxController.Axis.kRightX.value;
 
-  public static final CANdleSubsystem m_candleSubsystem = new CANdleSubsystem();
+  // public static final CANdleSubsystem m_candleSubsystem = new CANdleSubsystem();
   
-private static HashMap<String, Command> pathMap = new HashMap<>();
+  private static HashMap<String, Command> pathMap = new HashMap<>();
  
   private final SendableChooser<Command> _chooser = new SendableChooser<>();
 
   CommandXboxController driver = new CommandXboxController(Constants.GeneralConstants.DRIVER_CONTROLLER_ID);
   CommandXboxController operator = new CommandXboxController(Constants.GeneralConstants.OPERATOR_CONTROLLER_ID);
 
-  public void EndGameRumble() {
-
-    if(DriverStation.getMatchTime() < SwerveConstants.EndGameSeconds && DriverStation.getMatchTime() > SwerveConstants.StopRumbleSeconds) {
-       
-      driver.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0.5);
-      operator.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0.5);  
-    
-    } else {
-      driver.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0);
-      operator.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0);
-      
-    }
-  } 
+ 
 
   /* Operator Buttons */
   // private final JoystickButton cubeMode = new JoystickButton(operator.getHID(), XboxController.Button.kStart.value);
   // private final JoystickButton coneMode = new JoystickButton(operator.getHID(), XboxController.Button.kBack.value);
 
-  public final static SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  //public final static SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
   	// public static final CANdleSubsystem m_candleSubsystem = new CANdleSubsystem();
     public final Limelight m_limeLight = new Limelight();
 
+    public final Climber climber = new Climber();
 
 
   public RobotContainer() 
   {
-   
+
+    //configureDriverBindings();
+    configureOperatoreBindings();
+    // swerve.setDefaultCommand(new DriveFieldRelative(swerve, 
+    //                                                 () -> driver.getRawAxis(translationAxis)*-1,
+    //                                                 () -> driver.getRawAxis(strafeAxis) *-1,
+    //                                                 () -> driver.getRawAxis(rotationAxis)*-1)); 
     
-    configureBindings();
-    swerve.setDefaultCommand(new DriveFieldRelative(swerve, 
-                                                    () -> driver.getRawAxis(translationAxis)*-1,
-                                                    () -> driver.getRawAxis(strafeAxis) *-1,
-                                                    () -> driver.getRawAxis(rotationAxis)*-1)); 
-    
-           configureChooser();                                         
-                  
+          // configureChooser();   
+
+         climber.setDefaultCommand(climber.moveClimber(
+            MathUtil.applyDeadband(operator.getRawAxis(XboxController.Axis.kLeftY.value), .1),
+            MathUtil.applyDeadband(operator.getRawAxis(XboxController.Axis.kLeftX.value), .1)));
   }
 
-  private void configureBindings() 
+  // private void configureDriverBindings()
+  // {
+  //   driver.leftTrigger().onTrue(new InstantCommand(() -> swerve.zeroGyro()));
+  //   driver.a().whileTrue(new VisionAlignment(this::getXSpeed, 0, swerve));
+  //   driver.x().whileTrue(new LLAlignCommand(true));
+  //   driver.b().whileTrue(new DriveToDistance(swerve,60));
+  //   driver.povUp().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2)));
+  //   driver.povDown().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2 - Math.PI)));
+  //   driver.povLeft().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2 - Math.PI/2)));
+  //   driver.povRight().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2 + Math.PI/2)));
+  //   driver.leftBumper().onTrue(new InstantCommand(() -> swerve.setTranslationalScalar(Constants.SwerveConstants.SWERVE_SLOW_TRANSLATION)));
+  //   driver.leftBumper().onFalse(new InstantCommand(() -> swerve.setTranslationalScalar(Constants.SwerveConstants.SWERVE_NORMAL_TRANSLATION)));
+  // }
+
+  private void configureOperatoreBindings()
   {
-    driver.leftTrigger().onTrue(new InstantCommand(() -> swerve.zeroGyro()));
-    driver.a().whileTrue(new VisionAlignment(this::getXSpeed, 0, swerve));
-    driver.x().whileTrue(new LLAlignCommand(true));
-     driver.b().whileTrue(new DriveToDistance(swerve,60));
-    driver.povUp().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2)));
-    driver.povDown().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2 - Math.PI)));
-    driver.povLeft().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2 - Math.PI/2)));
-    driver.povRight().onTrue(new InstantCommand(() -> swerve.setHeadingAngle(Math.round(swerve.getYaw().getRadians() / (2.0*Math.PI)) * Math.PI * 2 + Math.PI/2)));
-    driver.leftBumper().onTrue(new InstantCommand(() -> swerve.setTranslationalScalar(Constants.SwerveConstants.SWERVE_SLOW_TRANSLATION)));
-    driver.leftBumper().onFalse(new InstantCommand(() -> swerve.setTranslationalScalar(Constants.SwerveConstants.SWERVE_NORMAL_TRANSLATION)));
-    operator.start().onTrue(new InstantCommand(() ->m_candleSubsystem.setLEDState(CANdleSubsystem.LEDState.CONE)));
-		operator.back().onTrue(new InstantCommand(() -> m_candleSubsystem.setLEDState(CANdleSubsystem.LEDState.CUBE)));
+    // operator.start().onTrue(new InstantCommand(() ->m_candleSubsystem.setLEDState(CANdleSubsystem.LEDState.CONE)));
+    // operator.back().onTrue(new InstantCommand(() -> m_candleSubsystem.setLEDState(CANdleSubsystem.LEDState.CUBE)));
     operator.x().onTrue(new InstantCommand(() ->  operator.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 1.0)));
+    operator.leftBumper().whileTrue(new InstantCommand(() -> climber.moveToMax(5)));
+    operator.rightBumper().whileTrue(new InstantCommand(() -> climber.moveToHome(5)));
   }
 
   double getXSpeed(){ 
@@ -130,16 +132,16 @@ private static HashMap<String, Command> pathMap = new HashMap<>();
     return finalY;
   } 
 
-  public void configureChooser() {
+  // public void configureChooser() {
         
-       //_chooser.setDefaultOption("Do Nothing", new InstantCommand());
-       _chooser.setDefaultOption("2M AUTO", new PathPlannerAuto("2M Auto"));
-       _chooser.addOption("5M Auto", new PathPlannerAuto("5M Auto"));
+  //      //_chooser.setDefaultOption("Do Nothing", new InstantCommand());
+  //      _chooser.setDefaultOption("2M AUTO", new PathPlannerAuto("2M Auto"));
+  //      _chooser.addOption("5M Auto", new PathPlannerAuto("5M Auto"));
 
-        SmartDashboard.putData("AUTO CHOICES ", _chooser); 
+  //       SmartDashboard.putData("AUTO CHOICES ", _chooser); 
         
         
-    }
+  //   }
 
    	/**
 	 * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -160,4 +162,19 @@ private static HashMap<String, Command> pathMap = new HashMap<>();
 
 	}
 	
+ public void EndGameRumble() {
+
+    if(DriverStation.getMatchTime() < SwerveConstants.EndGameSeconds && DriverStation.getMatchTime() > SwerveConstants.StopRumbleSeconds) {
+       
+      driver.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0.5);
+      operator.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0.5);  
+    
+    } else {
+      driver.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0);
+      operator.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0);
+      
+    }
+  } 
+
+
 }
