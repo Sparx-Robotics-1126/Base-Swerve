21904de2218f6dfdf9397c58da21184049fe029a
flareware95 <flareware95@gmail.com>
2024-02-14 20:33:29 -0500

    tuning

--- src/main/java/frc/team1126/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/team1126/subsystems/SwerveSubsystem.java
@@ -96,11 +96,9 @@ public class SwerveSubsystem extends SubsystemBase
         this::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
         this::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                                         new PIDConstants(5.0, 0.0, 0.0),
+                                         new PIDConstants(.7, 0.0, 0),
                                          // Translation PID constants
-                                         new PIDConstants(swerveDrive.swerveController.config.headingPIDF.p,
-                                                          swerveDrive.swerveController.config.headingPIDF.i,
-                                                          swerveDrive.swerveController.config.headingPIDF.d),
+                                         new PIDConstants(5,0,.01),
                                          // Rotation PID constants
                                          4.5,
                                          // Max module speed, in m/s
